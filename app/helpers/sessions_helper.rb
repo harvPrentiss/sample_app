69ab2122598e8e57434bc0dd5871b688eeedfa53
Harold Prentiss <harv.prentiss@gmail.com>
2013-12-28 22:35:43 -0500

    Fixes to user sign out.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,8 +32,8 @@ module SessionsHelper
     end
 
 	def sign_out
-		cookies.delete(:remember_token)
 		current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
+		cookies.delete(:remember_token)
 		self.current_user = nil
 	end
 
